3b9072459cdc03580feb41ad1c9342cf790e08b3
pengdashuai <xz87060361@126.com>
2020-06-09 18:36:46 +0800

    增加工具方法名

--- src/tool/StringTool.js
+++ src/tool/StringTool.js
@@ -8,11 +8,12 @@ class StringTool {
     /*
     校验是否为手机号码
     @param mobile 手机号码
-    @returns 结果 Booleans
+    @return 结果 Booleans
     */
     static isMobile(mobile) {
         return (/^1[1234567890]\d{9}$/.test(mobile));
     }
+
 }
 
 export default StringTool;
